5dd3e27d93d0dd62c9c59df9e0ec6a6d87661e95
Imri Neeman <neemanimri@gmail.com>
2021-07-13 09:20:42 -0700

    Step #2 - Add ability to publish ports & run detached services

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
+from json.decoder import JSONDecodeError
 from flask import Flask , request, jsonify
 from services import docker_service
 import json
 app = Flask(__name__)
 
-
+#Homepage
 @app.route("/")
 def homepage():
-    return "Mini Kubernetes!"
+    return "Mini-Kubernetes!"
 
-# Create new service
+# Create new services
 @app.route("/services", 
 methods = ['POST'])
 def create_service():
     if not request.data:
         return jsonify({'error':'Bad Request'}),400
-    data = json.loads(request.data)
+    try:
+        data = json.loads(request.data)
+    except JSONDecodeError:
+        return jsonify({'error':'Bad request'}),400
 
-    if 'image' not in data:
+    if 'image' not in data or not 'detached' in data or not 'publish' in data:
         return jsonify({'error':'Bad Request'}),400
     
-    res = docker_service.create(data['image'])
+    res = docker_service.create(data['image'],data['detached'],data['publish'])
     if res:
+        print(res)
         return jsonify(res),400
+
     return jsonify({'status': 'Successfuly created'}),201
 
 # Get latest service (optional - get latest by image)
