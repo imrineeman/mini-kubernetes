39f41a235130e965a21b623f2cc9d547154eff18
Imri Neeman <neemanimri@gmail.com>
2021-07-13 13:56:14 -0700

    Extended API

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ INTERVAL_TASK_ID = 'interval-task-id'
 
 scheduler.add_job(
     id=INTERVAL_TASK_ID,
-     func =docker_service.health_check,
+     func =docker_service.periodic_check,
       trigger='interval',
        seconds=10)
 
@@ -22,6 +22,55 @@ scheduler.add_job(
 @app.route("/")
 def homepage():
     return "Mini-Kubernetes!"
+
+# Get all services
+@app.route('/services')
+def get_all():
+    data = json.loads(docker_service.get_all())
+    return jsonify(data),200
+
+# Get all running services
+@app.route('/services/running')
+def get_all_running():
+    data = json.loads(docker_service.get_all())
+    filtered_data = []
+
+    for i in range(len(data)):
+        if data[i]['State'] == 'running':
+            filtered_data.append(data[i])
+
+    return jsonify(filtered_data),200
+
+# Get a specific service
+# Get by ID
+@app.route('/services/<id>')
+def get_by_id(id):
+    data = json.loads(docker_service.get_all())
+    filtered_data = []
+
+    for i in range(len(data)):
+        print(data[i])
+        if data[i]['ID'] == id:
+            filtered_data.append(data[i])
+    if not filtered_data:
+        return jsonify({'error':'Invalid ID'}),404
+    return jsonify(filtered_data),200
+
+# Get by name
+@app.route('/services/name/<name>')
+def get_by_name(name):
+    data = json.loads(docker_service.get_all())
+    filtered_data = []
+    print(data)
+    for i in range(len(data)):
+        print(data[i])
+        if data[i]['Names'] == name:
+            filtered_data.append(data[i])
+
+    if not filtered_data:
+        return jsonify({'error':'Invalid name'}),404
+
+    return jsonify(filtered_data),200
 
 # Create new services
 @app.route("/services", 
--- services/docker_service.py
+++ services/docker_service.py
@@ -1,8 +1,11 @@
-import subprocess
+import subprocess, logging
 
 def run_command(command):
     return subprocess.check_output(command, 
     shell=True)
+
+def get_all():
+    return run_command("echo ] | (docker ps -a --format '{{json .}}' | paste -sd',' && cat) | (echo [ && cat)")
 
 def get_latest(image):
     if not image:
@@ -13,7 +16,7 @@ def create(image,detached,publish):
     publish = '--publish-all' if publish is True else f'-p {publish}:80' if publish else ''
     if not detached:
         try:
-            subprocess.check_output(f'docker run {publish} {image}',shell=True)
+            run_command(f'docker run {publish} {image}')
         except subprocess.CalledProcessError:
             return {'error':'Command line error'}
         return 
@@ -24,6 +27,13 @@ def create(image,detached,publish):
         return {'error':'Command line error'}
     return 
 
-def health_check():
-    return run_command(f'docker-compose up -d')
-  
+def periodic_check():
+
+    run_command('docker-compose up -d')
+    #config_services =  run_command('docker-compose ps -q').decode("utf-8").split()
+    #running_services = run_command('docker inspect --format "{{.Id}}" $(docker ps -q)').decode("utf-8").split()
+
+ #   for i in running_services:
+ #       if i not in config_services:
+ #           run_command(f'docker stop {i}')
+ #           print('Killed service',i) 
